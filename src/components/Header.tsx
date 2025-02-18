c24306f352c402af42db9be409927b70dd1f3deb
Rohan Kumar Pandey <rohanpandey749@gmail.com>
2025-02-18 20:39:57 +0530

    changes made in opacity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,13 @@ import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
 import logo from "../assets/images/pirateslogo.svg";
 import icon from "../assets/images/logo.svg";
+import pirates from "../assets/images/pirates.svg";
 import Link from "next/link";
 import gsap from "gsap";
 import ReactHowler from "react-howler";
 import { MdOutlineMusicNote } from "react-icons/md";
 import { MdOutlineMusicOff } from "react-icons/md";
+import { usePathname } from "next/navigation";
 
 interface headerInterface {
   loading?: boolean;
@@ -17,6 +19,7 @@ interface headerInterface {
 const Header = ({ loading }: headerInterface) => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const [playing, setPlaying] = useState(true);
+  const router = usePathname()
 
   const toggleAudio = () => {
     // if (audioRef.current && !playing) {
@@ -37,6 +40,8 @@ const Header = ({ loading }: headerInterface) => {
       setPlaying(true)
     }
   }
+
+  console.log("Router:",router)
 
   
   useEffect(() => {
@@ -61,33 +66,32 @@ const Header = ({ loading }: headerInterface) => {
   }, []);
 
   return (
-    <header className="p-3 sm:p-5 lg:px-10 lg:p-10 header w-full opacity-0">
+    <header className="p-3 md:p-5 lg:px-10 lg:p-10 header w-full opacity-0">
     
       <div className="flex items-center justify-between">
-        <Link href={"/"} className="md:mr-[6rem]">
-          <Image src={logo} alt="Pirates" className="md:block hidden" />
-          <Image src={icon} alt="Pirates" className="md:hidden block" />
+        <Link href={"/"} className=" md:w-[14rem]">
+          <Image src={icon} alt="Pirates" className="" />
         </Link>
-        {/* <div className='hidden md:block'>
+        <div className='hidden md:block'>
         <Image src={pirates} alt='Pirates' />
-        </div> */}
-        <div className="flex items-center gap-4">
+        </div>
+        <div className="flex items-center gap-4  justify-end md:w-[14rem]">
         <Link href={""}>
           <button className="btn text-xs min-w-[8rem]">
              <span>CONNECT WALLET</span> 
             
           </button>
         </Link>
-          <span className="text-3xl text-[#C09D49] cursor-pointer hover:scale-[1.2] transition-all" onClick={toggleAudio}>
+          {router === "/" && <span className="text-3xl text-[#C09D49] cursor-pointer hover:scale-[1.2] transition-all" onClick={toggleAudio}>
             {playing ? <MdOutlineMusicNote /> : <MdOutlineMusicOff />}
-          </span>
+          </span>}
         </div>
       </div>
       {/* <audio controls>
   <source src="https://drive.google.com/file/d/11fcS_N8KffmrB-gAlPP7ITVwPTK0aUAC/view" type="audio/mpeg" />
   Your browser does not support the audio element.
 </audio> */}
-      <ReactHowler  playing={playing} src="/audio/audio.mp3"  rate={1}  loop={true} />
+      <ReactHowler mute={router !== "/" ? true : false} playing={playing} src="/audio/audio.mp3"  rate={1}  loop={true} />
       {/* <audio ref={audioRef} src="/clock.mp3" /> */}
     </header>
   );
--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -2,22 +2,28 @@
 
 import React from "react";
 import Button from "./common/Button";
+import dotshor from "../assets/images/dots-horizontal.png"
+import dotsver from "../assets/images/dots-vertical.png"
+import Image from "next/image";
+import ticket1 from "../assets/images/ticket1.png"
+import ticket2 from "../assets/images/ticket2.png"
+import ticket3 from "../assets/images/ticket3.png"
 
 const Mint = () => {
 
   const ticketOptions = [
     {
-        icon: "",
+        icon: <Image src={ticket1} alt="" />,
         amount: "1 Ticket",
         price: "Free"
     },
     {
-        icon: "",
+        icon: <Image src={ticket2} alt="" />,
         amount: "10 Tickets",
         price: "$2/Tickets"
     },
     {
-        icon: "",
+        icon: <Image src={ticket3} alt="" />,
         amount: "100 Tickets",
         price: "$20/Ticket"
     },
@@ -26,11 +32,15 @@ const Mint = () => {
 
   return (
     <section className="h-[70vh] w-full flex items-center justify-center z-50 relative overflow-x-hidden">
-      <div className="mintform p-2 sm:p-5 lg:p-10 text-center flex flex-col gap-3">
+      <div className="mintform relative p-2 sm:p-5 lg:p-14 text-center flex flex-col gap-3 bg-red-50">
+      <Image src={dotshor} alt="" className="absolute left-1/2 -translate-x-1/2 top-3" />
+      <Image src={dotshor} alt="" className="absolute left-1/2 -translate-x-1/2 bottom-3" />
+      <Image src={dotsver} alt="" className="absolute left-3 -translate-y-1/2 bottom-1/2" />
+      <Image src={dotsver} alt="" className="absolute right-3 -translate-y-1/2 bottom-1/2" />
         <h1>Golden Ticket Mint</h1>
-        <div className="">
+        <div className="flex justify-center">
           {" "}
-          <div className="w-full max-w-md">
+          <div className="w-full lg:w-[70%] max-w-md">
             <div className="w-full bg-gray-800 rounded-full h-1 relative overflow-hidden">
               <div
                 className="h-full bg-gradient-to-r progress from-yellow-700 via-yellow-600 to-yellow-400 transition-all duration-300"
@@ -40,12 +50,14 @@ const Mint = () => {
           </div>
         </div>
         <div className="text-sm">Remaining Ticketes: 40,000/100,000</div>
-        <div className="pt-5 grid grid-cols-3 gap-5">
+        <div className="pt-5 grid md:grid-cols-3 gap-5">
             {
                 ticketOptions.map((ticket) => {
                     return (
-                        <button key={ticket.price} className="mintOptions w-full h-full p-3 flex flex-col justify-between items-center">
-                            <div>h</div>
+                        <button key={ticket.price} className="mintOptions w-full h-full p-3 px-5 flex flex-col justify-between items-center">
+                            <div>
+                              {ticket.icon}
+                            </div>
                             <div className="text-sm">{ticket.amount}</div>
                             <div className="text-sm">{ticket.price}</div>
                         </button>
